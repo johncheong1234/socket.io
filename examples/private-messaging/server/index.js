d609cd491548a4878a87887c3e4ad49316933558
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-09-14 16:38:10 +0800

    localhost

--- examples/private-messaging/server/index.js
+++ examples/private-messaging/server/index.js
@@ -1,7 +1,7 @@
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://3.224.156.78:8080",
+    origin: "http://localhost:8080",
   },
 });
 
